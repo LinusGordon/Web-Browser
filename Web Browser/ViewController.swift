ff74bdd351ae831aaebd738017fb76bb2b6a8821
LinusGordon <linus.gordon@tufts.edu>
2015-12-26 13:27:34 -0800

    Web Browser
    This simple web browser allows you to navigate the web on any iOS
    device with a url bar, forward, and backwards button.
    Built in Swift

--- Web Browser/ViewController.swift
+++ Web Browser/ViewController.swift
@@ -8,18 +8,83 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var webView : UIWebView!
+    @IBOutlet var urlBar: UITextField!
+    @IBOutlet var goButton : UIButton!
+    @IBOutlet var backButton : UIButton!
+    @IBOutlet var forwardButton: UIButton!
+    
+    var lastPage : String!
+    var nextPage : String!
+    var currentPage : String!
+    var defaultWebpage = "https://www.google.com"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.urlBar.delegate = self
+        lastPage = defaultWebpage
+        currentPage = defaultWebpage
+        let url = NSURL(string:defaultWebpage)
+        let req = NSURLRequest(URL:url!)
+        self.webView!.loadRequest(req) //Loads the webpage
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func changePage(_: AnyObject)
+        
+    {
+        lastPage = currentPage
+        if urlBar.text!.hasPrefix("https://") {
+            urlBar.text = urlBar.text!
+        }
+        else {
+             urlBar.text = "https://" + urlBar.text!
+        }
+        currentPage = urlBar.text
+       
+        let url = NSURL(string:urlBar.text!)
+        let req = NSURLRequest(URL:url!)
+        self.webView!.loadRequest(req)
+        self.view.endEditing(true)
+    }
+   
+    @IBAction func goBack(_: AnyObject)
+    {
+        let url = NSURL(string:lastPage)
+        let req = NSURLRequest(URL: url!)
+        self.webView!.loadRequest(req)
+        nextPage = urlBar.text
+        urlBar.text = lastPage
+    }
+    
+    @IBAction func goForward(_: AnyObject)
+    {
+        if nextPage != nil
+        {
+            let url = NSURL(string:nextPage!)
+            let req = NSURLRequest(URL: url!)
+            self.webView!.loadRequest(req)
+            lastPage = urlBar.text
+            urlBar.text = nextPage
+        }
+        
+    }
+    
+    // Hides the keyboard
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        
+        self.view.endEditing(true);
+        
+        return false;
+        
+    }
+    
+    
 }
 
