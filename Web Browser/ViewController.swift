9172d16cce08dc33d59b33448cd542b2e6ee00f9
Linus Gordon <linus.gordon@tufts.edu>
2015-12-27 11:53:28 -0800

    Update ViewController.swift

--- Web Browser/ViewController.swift
+++ Web Browser/ViewController.swift
@@ -1,34 +1,22 @@
 //
 //  ViewController.swift
-//  Web Browser
+//  Tabs
 //
-//  Created by Linus Gordon on 12/25/15.
+//  Created by Linus Gordon on 12/27/15.
 //  Copyright © 2015 Linus. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController {
 
-    @IBOutlet var webView : UIWebView!
-    @IBOutlet var urlBar: UITextField!
-    @IBOutlet var goButton : UIButton!
-    @IBOutlet var backButton : UIButton!
-    @IBOutlet var forwardButton: UIButton!
-    
-    var lastPage : String!
-    var nextPage : String!
-    var currentPage : String!
-    var defaultWebpage = "https://www.google.com"
+    let password = "Guest"
+    @IBOutlet var passwordTextField : UITextField!
     
     override func viewDidLoad() {
+        self.navigationController!.navigationBarHidden = true;
         super.viewDidLoad()
-        self.urlBar.delegate = self
-        lastPage = defaultWebpage
-        currentPage = defaultWebpage
-        let url = NSURL(string:defaultWebpage)
-        let req = NSURLRequest(URL:url!)
-        self.webView!.loadRequest(req) //Loads the webpage
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,55 +24,15 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func changePage(_: AnyObject)
+    @IBAction func toView2(_: AnyObject) {
+        let view2 =  self.storyboard?.instantiateViewControllerWithIdentifier("View2") as! ViewController2
         
-    {
-        lastPage = currentPage
-        if urlBar.text!.hasPrefix("https://") {
-            urlBar.text = urlBar.text!
+        if password == passwordTextField.text {
+            self.navigationController?.pushViewController(view2, animated: true)
         }
-        else {
-             urlBar.text = "https://" + urlBar.text!
-        }
-        currentPage = urlBar.text
-       
-        let url = NSURL(string:urlBar.text!)
-        let req = NSURLRequest(URL:url!)
-        self.webView!.loadRequest(req)
-        self.view.endEditing(true)
     }
-   
-    @IBAction func goBack(_: AnyObject)
-    {
-        let url = NSURL(string:lastPage)
-        let req = NSURLRequest(URL: url!)
-        self.webView!.loadRequest(req)
-        nextPage = urlBar.text
-        urlBar.text = lastPage
-    }
-    
-    @IBAction func goForward(_: AnyObject)
-    {
-        if nextPage != nil
-        {
-            let url = NSURL(string:nextPage!)
-            let req = NSURLRequest(URL: url!)
-            self.webView!.loadRequest(req)
-            lastPage = urlBar.text
-            urlBar.text = nextPage
-        }
         
-    }
     
-    // Hides the keyboard
-    func textFieldShouldReturn(textField: UITextField!) -> Bool {
-        
-        self.view.endEditing(true);
-        
-        return false;
-        
-    }
-    
-    
-}
 
+
+}
